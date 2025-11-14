05f6c5cf72385c0eaec42908efaeb4ddf0553df5
igwebupower <john.igwe8844@gmail.com>
2025-11-14 06:04:43 +0000

    Add missing Link import

--- app/insights/page.tsx
+++ app/insights/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 import { Header } from '@/components/Header';
 import { InsightCard } from '@/components/InsightCard';
 import { getAllInsights, getFeaturedInsights } from '@/lib/insights';
